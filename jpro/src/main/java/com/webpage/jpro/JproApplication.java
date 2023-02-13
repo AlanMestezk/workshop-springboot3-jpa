fc0f8afe3a30a5f8650b2ef3e7683c1a2b6cf85f
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2023-02-13 16:18:17 -0300

    Seção 1: projeto criado
    Colocando em ação o primeiro projeto com springboot

---
+++ jpro/src/main/java/com/webpage/jpro/JproApplication.java
@@ -0,0 +1,13 @@
+package com.webpage.jpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JproApplication.class, args);
+	}
+
+}
---
+++ jpro/src/test/java/com/webpage/jpro/JproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webpage.jpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
